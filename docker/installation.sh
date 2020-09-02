300229d84569b37a37fee1a5abc575a738e477e6
Pablo Toledo Margalef <pabloatm980@gmail.com>
2020-09-02 10:21:28 -0300

    Remove nonsense comments

--- docker/installation.sh
+++ docker/installation.sh
@@ -16,9 +16,3 @@ luarocks make rockspec/*
 echo "Downloading source code"
 cd /root/vrn
 ./download.sh
-
-#Everything should be doable by the default user (fuck security amiright)
-#Installation is done by root for convenience
-#Give the `ubuntu` user ownership and permissions to everything installed
-#chown -R ubuntu /home/ubuntu /usr
-#chmod -R u+wrx /home/ubuntu /usr
